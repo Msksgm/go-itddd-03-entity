f67f9e96f7111496ad6509ed3a6f14847d2b6859
Msksgm <msksgm3124@gmail.com>
2022-02-17 11:54:50 +0900

    fix: UserName オブジェクトを削除したので、バリデーションを記述

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -15,9 +15,13 @@ func (user *User) NewUser(userId userId, userName userName) (*User, error) {
 	return &User{id: userId, name: userName}, nil
 }
 
-func (user *User) ChangeUserName(name userName) error {
-	if err := name.validate(name); err != nil {
-		return err
+func (user *User) ChangeUserName(name string) (err error) {
+	defer iterrors.Wrap(&err, "user.ChangeUserName(%q)", name)
+	if name == "" {
+		return fmt.Errorf("userName is required")
+	}
+	if len(name) < 3 {
+		return fmt.Errorf("userName %v is less than three characters long", name)
 	}
 	user.name = name
 	return nil
