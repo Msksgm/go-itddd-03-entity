a48cc7fb6c4ea953e8f1de241a8e4d1aef3ca213
Msksgm <msksgm3124@gmail.com>
2022-02-18 07:12:23 +0900

    fix: 変数をコマンドライン引数に変更

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/Msksgm/go-itddd-03-entity/domain/model/user"
 )
 
+var (
+	id   = flag.String("id", "", "id of user")
+	name = flag.String("name", "", "name of user")
+)
+
 func main() {
-	userId, err := user.NewUserId("a")
+	flag.Parse()
+	userId, err := user.NewUserId(*id)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	newUser, err := user.NewUser(*userId, "name")
+	newUser, err := user.NewUser(*userId, *name)
 	if err != nil {
 		log.Fatal(err)
 	}
